19085148df74b32a9281eba0b0119ed2c8fd8dc5
Phuong Tan Thanh <phuongtanthanh99@gmail.com>
2025-06-17 10:35:08 +0700

    fix: update Docker installation commands to include -y flag for non-interactive installation

--- cmd/docker.go
+++ cmd/docker.go
@@ -7,13 +7,13 @@ import "fmt"
 func installDocker() {
 	fmt.Println("Setting up Docker's apt repository")
 	commands := []string{
-		"sudo apt-get update",
+		"sudo apt-get -y update",
 		"sudo apt-get install ca-certificates curl",
 		"sudo install -m 0755 -d /etc/apt/keyrings",
 		"sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
 		"sudo chmod a+r /etc/apt/keyrings/docker.asc",
 		"echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"${UBUNTU_CODENAME:-$VERSION_CODENAME}\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-		"sudo apt-get update",
+		"sudo apt-get -y update",
 	}
 
 	for _, cmd := range commands {
@@ -21,7 +21,7 @@ func installDocker() {
 	}
 
 	fmt.Println("Installing Docker Packages")
-	runCommand("sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin")
+	runCommand("sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin")
 
 	fmt.Println("Adding user to docker group")
 	runCommand("sudo usermod -aG docker $USER")
